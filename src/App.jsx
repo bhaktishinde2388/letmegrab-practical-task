5d14a132306e7415d94d5635d998a444239553c7
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-04 23:33:38 +0530

    feature:setup routing in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './views/Home/Home'
+import Login from './views/Login/Login'
+import Signup from './views/Signup/Signup'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
