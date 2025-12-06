432573c5faf248bd553052f91df1202b713dcc03
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-06 16:54:28 +0530

    feat :  add prduct function is created

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import Login from './views/Login/Login'
 import Signup from './views/Signup/Signup'
 import Products from './views/Products/Products'
 
+
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route  path="/products" element={<Products />}/>
+      <Route path="/products" element={<Products/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/signup' element={<Signup/>}/>
     </Routes>
--- src/views/Products/Products.jsx
+++ src/views/Products/Products.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import Button from '../../components/Button/Button';
+
 
 function Products() {
+    const [products, setProducts] = useState([]);
+
+useEffect(() => {
+  const storedProducts = JSON.parse(localStorage.getItem("products")) || [];
+  setProducts(storedProducts);
+}, []);
+
+
+const addProduct = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const newProduct = {
+      id: Date.now(),
+      title: form.title.value,
+      price: form.price.value,
+      description: form.description.value,
+      category: form.category.value,
+    };
+
+}
   return (
-    <div>Products</div>
+    <div>
+          <form onSubmit={addProduct}>
+        <input type="text" name="title" placeholder="Product Title" required />
+        <input type="number" name="price" placeholder="Product Price" required />
+        <input type="text" name="description" placeholder="Product Description" required />
+        <input type="text" name="category" placeholder="Product Category" required />
+        <Button type="submit" text="Add Product" />
+      </form>
+    </div>
   )
 }
 
