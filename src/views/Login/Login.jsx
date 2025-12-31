e8da8d855636f87ecede9ec3d82a51644e9c2ebf
bhaktishinde2388 <bhaktishinde2388@gmail.com>
2025-12-31 19:16:22 +0530

    the login page is updated

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,21 +1,19 @@
-import React, { useState } from 'react'
-import Button from '../../components/Button/Button'
-import toast, { Toaster } from 'react-hot-toast'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import Button from '../../components/Button/Button';
+import toast, { Toaster } from 'react-hot-toast';
+import { Link, useNavigate } from 'react-router-dom';
+import './Login.css';
 
 function Login() {
-  const [user, setUser] = useState({
-    name: "",
-    password: "",
-  });
+  const [user, setUser] = useState({ name: "", password: "" });
+  const navigate = useNavigate();
 
   const login = () => {
     const users = JSON.parse(localStorage.getItem("users")) || [];
     const foundUser = users.find((u) => u.name === user.name);
 
     if (!foundUser) {
-      toast.error("User not found!")
+      toast.error("User not found!");
       return;
     }
 
@@ -24,42 +22,39 @@ function Login() {
       return;
     }
 
-
     localStorage.setItem("currentUser", JSON.stringify(foundUser));
-
     toast.success("Login Successful!");
     toast.loading("Redirecting...");
 
-  
+    setTimeout(() => {
+      navigate("/");
+    }, 2000);
+  };
 
-  setTimeout(() => {
-    window.location.href = '/';
-  }, 2000);
-};
-
-
-
-return (
-  <div className='signup-bg'>
-    <form className='forms-container' onSubmit={(e) => e.preventDefault()}>
-      <input className='usename-input input-box' type="text" placeholder='Enter UserName'
-        value={user.name}
-        onChange={(e) => setUser({ ...user, name: e.target.value })} />
-
-      <input className='password-input input-box' type="password" placeholder='Enter Password'
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })} />
-
-
-      <Button text='login' onClick={login} />
-
-      <p className="login-text">
-        Don't have an account? <Link to="/signup">Signup</Link>
-      </p>
-    </form>
-    <Toaster />
-  </div>
-)
+  return (
+    <div className='login-bg'>
+      <form className='login-form' onSubmit={(e) => e.preventDefault()}>
+        <h2>Login to Your Account</h2>
+        <input
+          type="text"
+          placeholder='Username'
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+        <input
+          type="password"
+          placeholder='Password'
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <Button text='Login' onClick={login} />
+        <p className="login-text">
+          Don't have an account? <Link to="/signup">Signup</Link>
+        </p>
+      </form>
+      <Toaster />
+    </div>
+  );
 }
 
 export default Login;
